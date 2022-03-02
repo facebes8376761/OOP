b733a7045aba406a59fc3d0aefa06dbfadeacfcf
DESKTOP-2738V26\Admin <besfedfestar@gmail.com>
2022-03-02 23:45:02 +0700

    Lab1

---
+++ HelloWorldProject/src/HelloWorldProject/main.java
@@ -0,0 +1,13 @@
+package HelloWorldProject;
+
+public class main {
+
+	public main() {
+		
+	}//end function
+
+	public static void main(String[] args) {
+		System.out.println( "Hello world" ) ;
+	}//end function
+
+}//end class
